742faf961e3ba08a1b2af822f3f65e696a744528
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-14 09:55:14 +0300

    Components

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">Covid-19 Visualizer</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.header {
+  font-family: 'Azonix', sans-serif;
+  font-size: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  word-wrap: break-word;
+}
+</style>
---
+++ src/components/Ratio.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="ratio">
+    <div class="ratio__confirmed">
+      <p class="ratio__procent">49.32%</p>
+    </div>
+    <div class="ratio__recovered">
+      <p class="ratio__procent">50.58%</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.ratio {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 50vh;
+  background-color: var(--color-white);
+
+  & > * {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__procent {
+    color: var(--color-white);
+    font-family: 'Azonix', sans-serif;
+    font-size: 4rem;
+  }
+
+  &__confirmed {
+    width: 49.32%;
+    height: 100%;
+    background: linear-gradient(
+        90deg,
+        var(--color-grey) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      var(--color-red);
+  }
+  &__recovered {
+    width: 50.68%;
+    height: 100%;
+    background: linear-gradient(
+        270deg,
+        var(--color-grey) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      var(--color-blue);
+  }
+}
+</style>
