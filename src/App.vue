bb09d3d9c703318a300617bf52df8b0db6217c5f
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-14 09:55:58 +0300

    Import components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header />
+    <section class="container">
+      <Ratio />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header';
+import Ratio from './components/Ratio';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Ratio
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('./assets/scss/main.scss');
 </style>
