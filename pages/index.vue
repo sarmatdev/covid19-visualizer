857e44017912dc275ffedf3c6e5ccb660a22e541
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-16 12:57:56 +0300

    minor

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,31 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        covid19-visualizer
-      </h1>
-      <h2 class="subtitle">
-        My grand Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="container">
+      <Header />
+      <PieChart :data="pieChartData" />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Header from '@/components/Header';
+import BarChart from '@/components/Charts/bar-chart';
+import PieChart from '@/components/Charts/pie-chart';
 export default {
   components: {
-    Logo
+    Header,
+    BarChart,
+    PieChart
+  },
+  async fetch({ store }) {
+    await store.dispatch('pie/getData');
+  },
+  computed: {
+    pieChartData() {
+      return this.$store.getters['pie/data'];
+    }
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
