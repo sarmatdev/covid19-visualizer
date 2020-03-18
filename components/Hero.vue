5782486810a4de75ce293eeccc0731b237585c35
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-18 19:06:14 +0300

    Create main hero comp

---
+++ components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <header class="header">
+      <h1 class="header__title">🦠Covid-19 Vizualizer</h1>
+    </header>
+    <Cards :data="cardData" />
+    <PieChart
+      :data="data"
+      :options="{ responsive: true, maintainAspectRatio: false }"
+    />
+  </section>
+</template>
+
+<script>
+import PieChart from '@/components/Charts/pie-chart';
+import Cards from '@/components/Cards';
+export default {
+  props: ['data', 'cardData'],
+  components: {
+    PieChart,
+    Cards
+  }
+};
+</script>
+
+<style></style>
